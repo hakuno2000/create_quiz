0be7ad89f94c33d1a43b68079a90a5895127c47d
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2022-01-01 20:23:05 +0700

    add question preview

--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -4,7 +4,7 @@
       :items="itemList"
       :expanded.sync="expanded"
       :single-expand="true"
-      item-key="item"
+      item-key="image"
       show-expand
       sort-by="number"
       class="elevation-1"
@@ -50,7 +50,7 @@
                   </v-col>
                 </v-row>
                 <v-row>
-                  <v-col v-for="option in editedItem.options" :key="option.text" cols="12" sm="12" md="12">
+                  <v-col v-for="option in editedItem.options" :key="option.index" cols="12" sm="12" md="12">
                     <v-text-field
                         v-model="option.text"
                         label="Answer"
@@ -110,19 +110,38 @@
         </v-dialog>
       </v-toolbar>
     </template>
-<!--    <template v-slot:expanded-item="{ headers, item }">-->
-<!--      <td :colspan="headers.length">-->
-<!--        <p></p>-->
-<!--        <v-textarea-->
-<!--            name="input-7-1"-->
-<!--            label="List of pictures"-->
-<!--            auto-grow-->
-<!--            outlined-->
-<!--            v-model="item.detail.items"-->
-<!--            @change="$emit('updateItems', item.detail.items)"-->
-<!--        ></v-textarea>-->
-<!--      </td>-->
-<!--    </template>-->
+    <template v-slot:expanded-item="{ item }">
+      <div style="display: flex; justify-content: center; margin: 20px">
+        <div class="quiz-container" id="quiz-container">
+          <div class="quiz-options d-flex justify-content-between">
+            <div class="correct-counter d-flex align-items-center">
+              <i class="far fa-check-circle icon mr-2"></i>
+              <!--          <strong>{{ questionsWithCorrectAnswers.length }}/{{ maxReachedQuestion+1 }}</strong>-->
+            </div>
+            <div class="mx-auto">
+              <h4 class="my-0">Câu số 1</h4>
+            </div>
+          </div>
+          <div class="quiz">
+            <section class="quiz--title">{{ item.question }}</section>
+            <!--        <span v-html="images[currentQuestion]"></span>-->
+            <div class="quiz-selections row justify-content-center gx-5">
+              <div class="col-12 col-md-5 mb-3 mx-md-4" v-for="(item, index) in item.options" :key="index">
+                <div class="quiz-selections--item">
+                  <section class="quiz-selections--letter mr-3">{{ getSelectionRepresentation(index + 1) }}</section>
+                  <p class="mx-0 mb-0 mt-2">{{ item.text }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!--        <hr class="mx-1 mx-md-5">-->
+          <!--        <div class="quiz-btn d-flex justify-content-end align-items-center">-->
+          <!--          <strong class="mr-2">{{ "Tiếp" }}</strong>-->
+          <!--          <i class="fas fa-arrow-circle-right icon-lg mr-2 mr-md-5"></i>-->
+          <!--        </div>-->
+        </div>
+      </div>
+    </template>
     <template v-slot:item.actions="{ item }">
       <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil</v-icon>
       <v-icon small @click="deleteItem(item)"> mdi-delete</v-icon>
@@ -164,20 +183,24 @@ export default {
       question: "",
       options: [
         {
+          index: 0,
           text: "Answer 1",
         },
         {
+          index: 1,
           text: "Answer 2",
         },
         {
+          index: 2,
           text: "Answer 3",
         },
         {
+          index: 3,
           text: "Answer 4",
         }
       ],
       answer_text: "",
-      answer : 0,
+      answer: 0,
     },
     defaultItem: {
       image: -1,
@@ -196,7 +219,7 @@ export default {
           text: "Answer 4",
         }
       ],
-      answer : 0,
+      answer: 0,
     },
   }),
 
@@ -292,10 +315,153 @@ export default {
 
     updateAnswer(value) {
       this.editedItem.answer = this.editedIndex.options.indexOf(value);
-    }
+    },
+
+    getSelectionRepresentation(index) {
+      return (index + 9).toString(36).toUpperCase()
+    },
   },
 };
 </script>
 
 <style scoped>
+.quiz-container {
+  padding: 20px 30px;
+  box-shadow: rgba(0, 0, 0, 0.2) 1px 3px 8px 1px;
+  border-radius: 8px;
+  color: white;
+  background-color: #FBAB7E;
+  background-image: linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%);
+  min-height: 500px;
+  position: relative;
+}
+
+.quiz--title {
+  font-size: 1.8em;
+  min-height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  text-align: center;
+}
+
+.quiz-selections--letter {
+  font-weight: bold;
+  font-size: 1.2em;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: orange;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+.quiz-selections--item {
+  display: flex;
+  padding: 10px;
+  background: rgba(255, 255, 255, 0.6);
+  border-radius: 50px;
+  transition: all .2s ease;
+}
+
+.quiz-selections--item:hover {
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+
+.quiz-selections--item p {
+  color: #212124;
+  max-width: 75%;
+  text-align: justify;
+}
+
+.quiz-selections__correct {
+  background-color: #53E691;
+}
+
+.quiz-selections__correct:hover {
+  background-color: #53E691;
+}
+
+.quiz-selections__wrong {
+  background-color: #ED524E;
+}
+
+.quiz-selections__wrong:hover {
+  background-color: #ED524E;
+}
+
+.quiz-selections__correct p,
+.quiz-selections__wrong p {
+  color: white;
+  font-weight: bold;
+}
+
+.quiz-btn:hover {
+  cursor: pointer;
+}
+
+.correct-counter {
+  position: absolute;
+}
+
+.quiz--complete {
+  height: 100%;
+  position: absolute;
+  width: 80%;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+
+.redo {
+  color: #ED524E;
+  transition: all .2s ease;
+}
+
+.redo:hover {
+  cursor: pointer;
+  color: white;
+}
+
+.correct-text {
+  color: #53E691;
+  font-weight: bold;
+  font-size: 2em;
+}
+
+.icon {
+  font-size: 1.5em;
+}
+
+.icon-lg {
+  font-size: 1.8em;
+}
+
+.mw-100 {
+  max-width: 100%;
+}
+
+hr {
+  border: none;
+  height: 1px;
+  color: white;
+  background-color: white;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.quiz--img {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  margin-bottom: 30px;
+  max-height: 300px;
+  margin-top: -30px;
+}
 </style>
